b705898c6dff24d3671bf2934929f43197c71838
Eduard1992hardcode <edchhe@yandex.ru>
2020-06-09 12:41:19 +0300

    прообраз

---
+++ VietClinic/VietClinic/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VietClinic.Models
+{
+    abstract class Pet
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Weight { get; set; }
+    }
+}
