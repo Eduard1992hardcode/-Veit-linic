bad6aefec8c28ade40cce93b660fefa1242107b6
Eduard1992hardcode <edchhe@yandex.ru>
2020-06-11 10:30:41 +0300

    описание и реализация сущности Owner

---
+++ VietClinic/VietClinic/Services/IOwnerSertvice.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VietClinic.Models;
+
+namespace VietClinic.Services
+{
+    public interface IOwnerService
+    {
+        Task<List<Owner>> GetOwners();
+        Task<Owner> GetOwner(long id);
+        Task<Owner> EditOwner(Owner owner, long id);
+        Task<Owner> AddOwner(Owner owner);
+        Task<bool> DeletePet(long id);
+    }
+}
---
+++ VietClinic/VietClinic/Services/OwnerSertvice.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VietClinic.Models;
+
+namespace VietClinic.Services
+{
+    public class OwnerSertvice : IOwnerService
+    {
+        private readonly DataContext _context;
+
+        public OwnerService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Owner> AddOwner(Owner owner)
+        {
+            _context.Owners.Add(owner);
+            await _context.SaveChangesAsync();
+            return owner;
+        }
+
+        public async Task<bool> DeletePet(long id)
+        {
+            var owner = await _context.Owners.FindAsync(id);
+            if (owner == null)
+            {
+                return false;
+            }
+            _context.Owners.Remove(owner);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Owner> EditOwner(Owner owner, long id)
+        {
+            var petFromDb = await _context.Owners.FindAsync(id);
+
+            if (petFromDb == null)
+            {
+                return owner;
+            }
+
+            petFromDb.Adress = owner.Adress;
+            petFromDb.TelNumber = owner.TelNumber;
+            
+
+            await _context.SaveChangesAsync();
+
+            return petFromDb;
+        }
+
+        public async Task<Owner> GetOwner(long id)
+        {
+            return await _context.Owners.FindAsync(id);
+        }
+
+        public async Task<List<Owner>> GetOwners()
+        {
+            return await _context.Owners.ToListAsync();
+        }
+    }
